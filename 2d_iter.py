9b748acf610acfaa568ffd3c0976bbb4347311b2
Taj Dyson <taj.dyson@gmail.com>
2020-03-04 21:57:27 -0500

    now do a secondary SVD after first cleaning

--- 2d_iter.py
+++ 2d_iter.py
@@ -6,7 +6,8 @@ import datetime
 data_dir = "/home/wizard/mars/data_auto_cross"
 plot_dir = "/home/wizard/mars/plots"
 sensitivity = 5 # anything sensitivity*MAD above/below median flagged
-nmode = 3 # number of SVD modes to subtract off
+first_nmode = 3 # number of SVD modes to subtract off to start with
+second_nmode = 3 # after initial flagging
 max_iters = 50
 
 ti = 1562813825
@@ -18,15 +19,16 @@ subject = data[0] # BE SURE TO LOOK AT THE POL11 STUFF TOO!!
 
 logdata = np.log10(subject)
 u, s, v = np.linalg.svd(logdata, 0)
-first_modes = np.matmul(u[:,:nmode], np.matmul(np.diag(s[:nmode]), v[:nmode,:]))
+first_modes = np.matmul(u[:,:first_nmode], np.matmul(np.diag(s[:first_nmode]), v[:first_nmode,:]))
 corrected = logdata - first_modes
 
 plt.plot(np.log10(s), 'k.')
 plt.savefig("10th_svd")
 
-rfi_removedt = logdata.copy()
-rfi_removedf = logdata.copy()
+rfi_removedt = logdata.copy() #remove in final version
+rfi_removedf = logdata.copy() #remove in final version
 rfi_removed = logdata.copy()
+rfi_replaced = corrected.copy()
 
 flags = np.zeros_like(logdata, dtype=bool)
 
@@ -34,28 +36,49 @@ last_flags = 0
 
 for i in range(max_iters):
 
+    if i == 1:
+        # second SVD, without RFI
+        gapfilled_logdata = rfi_replaced + first_modes
+        u, s, v = np.linalg.svd(gapfilled_logdata, 0)
+        gapfilled_first_modes = np.matmul(u[:,:second_nmode], np.matmul(np.diag(s[:second_nmode]), v[:second_nmode,:]))
+        rfi_replaced = gapfilled_logdata - gapfilled_first_modes
+
+        plt.plot(np.log10(s), 'k.')
+        plt.savefig(f"{plot_dir}/10th_second_svd")
+        plt.clf()
+
     plt.title(f"RFI removed after {i} iterations")
     plt.imshow(rfi_removed, aspect='auto')
     plt.colorbar()
-    plt.savefig(f"{plot_dir}/10th_rfi_iter_{i}", dpi=600)
+    plt.savefig(f"{plot_dir}/10th_rfi_iter_{i}")
     plt.clf()
     
     plt.title(f"gapfilled data after {i} iterations")
-    plt.imshow(corrected, aspect='auto')
+    plt.imshow(rfi_replaced, aspect='auto')
     plt.colorbar()
-    plt.savefig(f"{plot_dir}/10th_data_iter_{i}", dpi=600)
+    plt.savefig(f"{plot_dir}/10th_data_iter_{i}")
     plt.clf()
     
-    mediant = np.median(corrected, axis=0) # calculate median using replaced values
-    minus_medt = logdata - mediant # here use raw data
+    mediant = np.median(rfi_replaced, axis=0) # calculate median using replaced values
+    minus_medt = corrected - mediant # here use raw data minus SVD baseline (RFI present)
     # It'll be masked out in the MAD calculation, and it'll let flagged points stay flagged between iterations
     MADt = np.ma.median(np.abs(np.ma.masked_where(flags, minus_medt)), axis=0) # use only points that haven't been flagged (yet)
     # now have (freq) values to be compared to each time-dependent column
     
-    medianf = np.median(corrected, axis=1)
+    medianf = np.median(rfi_replaced, axis=1)
     minus_medf = corrected - medianf.reshape((-1,1)) # effectively a transpose
     MADf = np.ma.median(np.abs(np.ma.masked_where(flags, minus_medf)), axis=1)
     # (time) values to be compared to each freq-dependent row
+
+    chan = 1500 + 101
+    
+    plt.title(f"RFI removed in channel {chan} after {i} iterations")
+    plt.plot(rfi_removedt[:,chan] - first_modes[:,chan])
+    plt.plot(MADt[chan]*sensitivity*np.ones_like(rfi_removed[:,chan]), label="MADt")
+    plt.plot(mediant[chan]*np.ones_like(rfi_removed[:,chan]), label="mediant")
+    plt.legend()
+    plt.savefig(f"{plot_dir}/10th_rfi_channel_{chan}_iter_{i}")
+    plt.clf()
     
     flagst = (np.abs(minus_medt) > sensitivity * MADt)
     flagsf = (np.abs(minus_medf) > (sensitivity * MADf).reshape((-1,1)))
@@ -66,12 +89,17 @@ for i in range(max_iters):
     rfi_removedf = np.ma.masked_where(flagsf, rfi_removedf)
     rfi_removed = np.ma.masked_where(flags, rfi_removed)
 
-    print(np.sum(flags) - last_flags)
-    if (np.sum(flags) - last_flags < logdata.size * (1 - 0.997)):
+    total_flags = np.sum(flags)
+
+    print(total_flags - last_flags)
+    if (total_flags - last_flags < logdata.size * (1 - 0.997)):
         print(f"converged after {i} iters")
+        # I don't plot the results as the flagged points were just statistical fluctuations
         break
-	
-    corrected[flags] = 0 # effectively setting them to background level
+    
+    rfi_replaced[flags] = 0 # effectively setting them to background level
+
+    last_flags = total_flags
     
     
 else:
