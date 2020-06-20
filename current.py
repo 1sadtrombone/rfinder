6f75bab3976e0065d56f3e2be6c0b5be1ce134a4
Taj Dyson <taj.dyson@gmail.com>
2020-06-20 13:10:48 -0400

    Trying making the median that of the lowpassed data, then doing MAD on highpassed data.

--- current.py
+++ current.py
@@ -7,8 +7,9 @@ import datetime
 data_dir = "/home/wizard/mars/data_auto_cross"
 plot_dir = "/home/wizard/mars/plots/rfinder"
 times_file = "/home/wizard/mars/scripts/rfinder/good_times.csv"
-name = "highpass_abs_5MAD" # string to identify plots saved with these settings
+name = "highpass_med_of_lowpass" # string to identify plots saved with these settings
 sensitivity = 5 # anything sensitivity*MAD above/below median flagged
+fmode = 2
 
 times = np.genfromtxt(times_file)
 
@@ -35,8 +36,10 @@ plt.savefig(f"{plot_dir}/{name}_logdata", dpi=600)
 plt.clf()
 
 fourier = np.fft.fft(logdata, axis=0)
-fourier[:10] = 0
+fourier[fmode:] = 0
 filtered = np.fft.ifft(fourier, axis=0)
+
+corrected = logdata - np.abs(filtered)
 
 plt.imshow(np.real(filtered[:,plot_if:plot_ff]), aspect='auto')
 plt.colorbar()
@@ -44,34 +47,22 @@ plt.savefig(f"{plot_dir}/{name}_filtered")
 plt.clf()
 
 mediant = np.median(filtered, axis=0)
-minus_medt = filtered - mediant
+minus_medt = corrected - mediant
 MADt = np.median(np.abs(minus_medt), axis=0)
 # now have (freq) values to be compared to each time-dependent column
 
-plt.plot(np.real(filtered[:,200]))
-plt.plot((MADt*sensitivity+np.abs(mediant))[200]*np.ones_like(logdata[:,500]))
-plt.plot((-MADt*sensitivity+np.abs(mediant))[200]*np.ones_like(logdata[:,500]))
-plt.savefig(f"{plot_dir}/{name}_filt_200")
-plt.clf()
-
-plt.plot(np.real(filtered[:,500]))
-plt.plot((MADt*sensitivity+np.abs(mediant))[500]*np.ones_like(logdata[:,500]))
-plt.plot((-MADt*sensitivity+np.abs(mediant))[500]*np.ones_like(logdata[:,500]))
-plt.savefig(f"{plot_dir}/{name}_filt_500")
+plt.plot(np.real(corrected[:,1541]))
+plt.plot((MADt*spensitivity+np.abs(mediant))[1541]*np.ones_like(logdata[:,500]))
+plt.plot((-MADt*sensitivity+np.abs(mediant))[1541]*np.ones_like(logdata[:,500]))
+plt.savefig(f"{plot_dir}/{name}_filt_1541")
 plt.clf()
 
 flags = (np.abs(minus_medt) > sensitivity * MADt)
 
-rfi_removed = np.ma.masked_where(flags, filtered)
+rfi_removed = np.ma.masked_where(flags, corrected)
 
 rfi_occ_freq = np.sum(flags, axis=0) / flags.shape[0]
 rfi_occ_time = np.sum(flags, axis=1) / flags.shape[1]
-
-plt.title("RFI removed")
-plt.imshow(np.real(rfi_removed[:,plot_if:plot_ff]), aspect='auto')
-plt.colorbar()
-plt.savefig(f"{plot_dir}/{name}_rfi_removed_real", dpi=600)
-plt.clf()
 
 plt.title("RFI removed")
 plt.imshow(np.abs(rfi_removed[:,plot_if:plot_ff]), aspect='auto')
@@ -84,6 +75,8 @@ plt.imshow(np.ma.masked_where(flags, logdata)[:,plot_if:plot_ff], aspect='auto')
 plt.colorbar()
 plt.savefig(f"{plot_dir}/{name}_rfi_removed_logdata", dpi=600)
 plt.clf()
+
+
 
 f, ((a0, a1), (a2, a3)) = plt.subplots(2, 2, gridspec_kw={'width_ratios': [3,1], 'height_ratios':[1,3]})
 a1.set_axis_off()
