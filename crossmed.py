d9a8fabeb3f6e8bf398c10bee9ed07d4f73d7335
Taj Dyson <taj.dyson@gmail.com>
2020-07-31 13:58:04 -0400

    Now median filter the channel medians before subtracting, to not wrongly subtract off any outliers (very RFI dense channels). May now flag some unwanted channels, and we may just have to take the hit of not flagging that one dense channel in [9]. To do so, flag bin by bin instead of globally (and then filtering the medians becomes pointless, so just don't do that either).

--- crossmed.py
+++ crossmed.py
@@ -9,16 +9,16 @@ data_dir = "/home/wizard/mars/data_auto_cross"
 plot_dir = "/home/wizard/mars/plots/rfinder"
 times_file = "/home/wizard/mars/scripts/rfinder/good_times.csv"
 
-sensitivity = 5 # anything sensitivity*MAD above/below median flagged
+sensitivity = 10 # anything sensitivity*MAD above/below median flagged
+med_win = 5
 window = 25 # median filter window length
-medians_win = 10 # median filter of channel medians window length
 
-name = f"crossmed_anotherday_{sensitivity}MAD_{window}win" # string to identify plots saved with these settings
+name = f"crossmed_filteredmeds_anotherday_{sensitivity}MAD_{window}win_{med_win}medwin" # string to identify plots saved with these settings
 
 times = np.genfromtxt(times_file)
 
-ti = times[6]
-tf = times[7]
+ti = times[18]
+tf = times[19]
 
 time, data = sft.ctime2data(data_dir, ti, tf)
 
@@ -32,7 +32,7 @@ plot_if = 0
 plot_ff = 2100
 
 t = 250
-f = 1221
+f = 1572
 
 logdata = np.log10(subject[:,startf:])
 
@@ -55,11 +55,18 @@ exit()
 
 median_f = np.median(logdata, axis=0)
 
-minus_meds = logdata - median_f
+filtered_meds = median_filter(median_f, med_win)
+
+minus_meds = logdata - filtered_meds
 
 filtered = median_filter(minus_meds, [1, window])
 
 corrected = minus_meds - filtered
+
+plt.imshow(corrected[:,plot_if:plot_ff], aspect='auto', vmin=-0.0025, vmax=0.0025)
+plt.colorbar()
+plt.savefig(f"{plot_dir}/{name}_corrected", dpi=600)
+plt.clf()
 
 plt.plot(median_f[plot_if:plot_ff])
 plt.savefig(f"{plot_dir}/{name}_median_f")
@@ -70,27 +77,8 @@ plt.colorbar()
 plt.savefig(f"{plot_dir}/{name}_minus_meds")
 plt.clf()
 
-plt.imshow(corrected[:,plot_if:plot_ff], aspect='auto', vmin=-0.0025, vmax=0.0025)
-plt.colorbar()
-plt.savefig(f"{plot_dir}/{name}_corrected", dpi=600)
-plt.clf()
-
 MAD = np.median(np.abs(corrected), axis=0)
 
-axes = plt.gca()
-axes.set_ylim([-0.01,0.01])
-plt.plot(corrected[:,f])
-plt.plot(np.median(corrected[:,f])*np.ones_like(logdata[:,500]))
-plt.plot((MAD*sensitivity))
-plt.savefig(f"{plot_dir}/{name}_corrected_{f}f", dpi=600)
-plt.clf()
-
-plt.plot(corrected[t])
-plt.plot(np.median(corrected[t])*np.ones_like(logdata[500]))
-plt.plot((MAD*sensitivity*np.ones_like(logdata[500])))
-plt.savefig(f"{plot_dir}/{name}_corrected_{t}t", dpi=600)
-plt.clf()
-         
 flags = (corrected > sensitivity * MAD)
 
 rfi_removed = np.ma.masked_where(flags, corrected)
@@ -98,6 +86,22 @@ rfi_removed = np.ma.masked_where(flags, corrected)
 rfi_occ_freq = np.sum(flags, axis=0) / flags.shape[0]
 rfi_occ_time = np.sum(flags, axis=1) / flags.shape[1]
 
+axes = plt.gca()
+axes.set_ylim([-0.01,0.01])
+plt.plot(corrected[:,f] - np.median(corrected[:,f]))
+plt.plot(np.arange(corrected[:,f].size)[np.where(flags[:,f])], (corrected[:,f]-np.median(corrected[:,f]))[np.where(flags[:,f])], 'r.')
+plt.plot((MAD[f]*np.ones_like(logdata[:,500])*sensitivity))
+plt.plot((MAD[f]*np.ones_like(logdata[:,500])))
+plt.savefig(f"{plot_dir}/{name}_corrected_{f}f", dpi=600)
+plt.clf()
+
+plt.plot(corrected[t])
+plt.plot(np.median(corrected[t])*np.ones_like(logdata[500]))
+plt.plot((MAD*sensitivity*np.ones_like(logdata[500])))
+plt.savefig(f"{plot_dir}/{name}_corrected_{t}t", dpi=600)
+plt.show()
+plt.clf()
+         
 plt.title("RFI removed")
 plt.imshow(rfi_removed[:,plot_if:plot_ff], aspect='auto', vmin=-.001, vmax=.001)
 plt.colorbar()
@@ -118,7 +122,7 @@ a0.margins(0)
 a3.plot(rfi_occ_time, np.arange(rfi_occ_time.size))
 a3.margins(0)
 a3.set_ylim(a3.get_ylim()[::-1])
-a2.imshow(rfi_removed, aspect='auto', vmin=0, vmax=np.max(rfi_removed))
+a2.imshow(rfi_removed, aspect='auto', vmin=-0.001, vmax=0.001)
 
 plt.title("RFI occupancy")
 plt.tight_layout()
